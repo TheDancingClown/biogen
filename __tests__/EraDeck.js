83390270c19f10400876337481b58d31c1751175
Louis Kirkham <chew@talk21.com>
2020-12-17 21:38:16 +0000

    event card list added

---
+++ __tests__/EraDeck.js
@@ -0,0 +1,15 @@
+import EraDeck from '../src/EraDeck';
+
+const eraDeck = new EraDeck()
+
+beforeEach(() => {
+  jest.spyOn(global.Math, 'random').mockReturnValue(.1);
+});
+
+afterEach(() => {
+  jest.spyOn(global.Math, 'random').mockRestore();
+});
+
+// it('draws a random event', () => {
+//   expect(eraDeck.draw('hadean')).toEq('Mars Paleo-Ocean')
+// })
---
+++ src/EraDeck.js
@@ -0,0 +1,14 @@
+
+
+class EraDeck {
+
+  constructor() {
+    const hadean = [37, 38, 39, 40, 41, 42]
+    const archean = [43, 44, 45, 46, 47, 48, 49]
+    const proterozoic = [50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60]
+  }
+
+  draw(era) {
+    
+  }
+}
