94a696fad77a6dd651cc8359d1b8997936f6d861
Louis Kirkham <chew@talk21.com>
2020-12-30 17:57:41 +0000

    added information alerts to close up view

--- components/RefugiumCard.js
+++ components/RefugiumCard.js
@@ -4,17 +4,29 @@ import { Manna, LifeDice, Enzymes } from './LargeRefugiumComponents';
 
 const RefugiumCard = (props) => {
 
-  // const landformAlert = () => {
-  //   Alert.alert(
-  //     "The landform icons for the current event. Active landforms are shown in black and inactive landforms are  shown in grey.\n\nAn active landform means you can assign Bionts and Enzymes to Refugia in its row."
-  //   )
-  // }
+  const organisedMannaAlert = () => {
+    Alert.alert(
+      "The top of the Refugium displays organised Manna. This is a combination of any assigned player Bionts as well as any Manna that has been animated through the Autocatalytic Roll. If a double is rolled, life can be created and the organised Manna will become the chromosomes of the microorganism"
+    )
+  }
 
-  // const climateAlert = () => {
-  //   Alert.alert(
-  //     "The climate icon(s) for the current event. The current climate is used to determine which dice animate life (organise Manna). If no icon is shown the previous climate remains."
-  //   )
-  // }
+  const lifeDiceAlert = () => {
+    Alert.alert(
+      "Any result during the Autocatalytic Roll that matches the Life Dice to the right of the current climate will animate Manna. Choose a Manna cube moving it from disorganised at the bottom to organised at the top. "
+    )
+  }
+
+  const enzymesAlert = () => {
+    Alert.alert(
+      "Any result during the Autocatalytic Roll that matches the Death Dice as shown will cause Manna death. Choose a Manna cube moving it from organised at the top to disorganised at the bottom, or choose a Biont to return to that player's tabelau. You are compensated with a catalyst of the same colour (or choose the colour if you removed your biont).\n\nIf the symbol also displays a red cross this causes Enzyme death and the rightmost Enzyme is lost.\n\nIf the Refugium is contested, the Progenote(based on dominance of organised Manna and Enzyme colour) decides Manna life and death but compensation is given to another contestant."
+    )
+  }
+
+  const disorganisedMannaAlert = () => {
+    Alert.alert(
+      "The bottom of the Refugium displays disorganised Manna. Manna can be destroyed in Smite events or organised during the Autocatalytic Rolls. Disorganised Manna is lost during the creation of microorganisms."
+    )
+  }
 
   // const eventAlert = () => {
   //   const events = [...new Set(props.card.event)];
@@ -28,11 +40,19 @@ const RefugiumCard = (props) => {
   return (
     <View style={styles.container}>
       <View style={styles.detail}>
-        <Manna manna={props.refugium.organisedManna} />
+        <TouchableOpacity onPress = {organisedMannaAlert}>
+          <Manna manna={props.refugium.organisedManna} />
+        </TouchableOpacity>
         <Text style={[styles.refugiaText,{ color: `${props.refugium.colour}`}]}>{props.refugium.title}</Text>
-        <LifeDice dice={props.refugium.lifeDice}/>
-        <Enzymes enzymes={props.refugium.enzymes}/> 
-        <Manna manna={props.refugium.manna}/> 
+        <TouchableOpacity onPress = {lifeDiceAlert}>
+          <LifeDice dice={props.refugium.lifeDice}/>
+        </TouchableOpacity>
+        <TouchableOpacity onPress = {enzymesAlert}>
+          <Enzymes enzymes={props.refugium.enzymes}/>
+        </TouchableOpacity>
+        <TouchableOpacity onPress = {disorganisedMannaAlert}>
+          <Manna manna={props.refugium.manna}/>
+        </TouchableOpacity>
       </View>
     </View>
   );
